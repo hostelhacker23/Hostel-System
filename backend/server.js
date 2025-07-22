6e85b17e17a1c0855619bffd7c60953204bd8ef9
mrdeveloper917 <thakurayush995537@gmail.com>
2025-07-23 01:32:41 +0530

    modified some backend code

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const User = require("./models/User");
+const User = require("../backend/models/User.js");
 
 const app = express();
 const PORT = 5500;
